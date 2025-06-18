27b08cddad360361e998d24c002add7de90bec15
NorthRueangsak <notdbk@gmail.com>
2025-06-18 11:05:49 +0700

    first commit

---
+++ w01.py
@@ -0,0 +1,15 @@
+# print("Hello world")
+
+# ตัวแปรจำนวนเต็ม
+number_int = 13
+# ตัวแปรทศนิยม
+number_float =3.14
+print(number_float)
+txt = "Rueangsak Noenchaiyaphum"
+print(txt)
+is_online = True
+power_on = False
+print(is_online)
+
+print(type(number_float))
+print(type(number_int))
